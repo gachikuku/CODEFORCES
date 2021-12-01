aebe534b62c30bd7bf2fc24d18ab07fe3fb83a56
Alnoman Kamil <alneihzen@gmail.com>
2021-12-01 16:31:48 +0200

    Add files via upload

---
+++ 4A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve(){
+
+    int w;
+    
+    cin >> w;
+    
+    if(w <= 2 || w % 2 != 0) {cout << "NO\n";}
+    
+    else {cout << "YES\n";}
+}
+
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie();
+    solve();
+    return 0;
+}
