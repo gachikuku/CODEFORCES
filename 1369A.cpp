2cd62769c12d9fcf0580ddc1dc23a11cf9727a0c
Alnoman Kamil <alneihzen@gmail.com>
2021-12-21 12:45:43 +0200

    Add files via upload

---
+++ 1369A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+
+void solve(){
+
+    ll n=0;
+    
+    cin>>n;
+    
+    if(n%4==0) cout<<"YES\n";
+    
+    else cout<<"NO\n";
+}
+
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie();
+    
+    int t;
+    
+    cin>>t;
+    
+    while(t--) solve();
+}
